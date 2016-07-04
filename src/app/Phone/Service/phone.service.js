d2abe0b8a7c80e3dc4af6d109fb303934207917f
Vignesh Sakthivel <vignesh.sakthivel@aspiresys.com>
2016-07-04 07:17:10 -0700

    Add datasource - set of JSON files for the application

--- src/app/Phone/Service/phone.service.js
+++ src/app/Phone/Service/phone.service.js
@@ -1,7 +1,7 @@
 angular.module('app.phone').
   factory('Phone', ['$resource',
     function($resource) {
-      return $resource('/phones/:phoneId.json', {}, {
+      return $resource('../phones/:phoneId.json', {}, {
         query: {
           method: 'GET',
           params: {phoneId: 'phones'},
